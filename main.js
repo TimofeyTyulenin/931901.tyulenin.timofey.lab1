3695eda4b1bf9666dbb138ff98722c2dbc1fc143
Timofey Tyulenin <tiulenko36@gmail.com>
2021-11-15 20:49:37 +0700

    newjs

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-let popupbg = document.querySelector('.popup_bg');
-let popupf = document.querySelector('.popupf');
-let popups = document.querySelector('.popups');
-let popupt = document.querySelector('.popupt');
-
-
 let btn1 = document.querySelector('.btn1');
 let btn2 = document.querySelector('.btn2');
 let btn3 = document.querySelector('.btn3');
 
+
+
 btn1.onclick=function(){
-    popupbg.classList.add('active');
-    popupf.classList.add('active');
+    var elem = document.getElementsByClassName("catn");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='850px';
+	var elem = document.getElementsByClassName("dogn");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='50px';
+	var elem = document.getElementsByClassName("cat");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='600px';
+	var elem = document.getElementsByClassName("dog");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='0px';
 };
 btn2.onclick=function(){
-    popupbg.classList.add('active');
-    popups.classList.add('active');
+    var elem = document.getElementsByClassName("catn");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='450px';
+	var elem = document.getElementsByClassName("dogn");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='450px';
+	var elem = document.getElementsByClassName("cat");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='410px';
+	var elem = document.getElementsByClassName("dog");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='410px';
+    
 };
 btn3.onclick=function(){
-    popupbg.classList.add('active');
-    popupt.classList.add('active');
+    var elem = document.getElementsByClassName("dogn");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='850px';
+	var elem = document.getElementsByClassName("catn");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='50px';
+	var elem = document.getElementsByClassName("dog");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='600px';
+	var elem = document.getElementsByClassName("cat");
+	for(var i=0; i<elem.length; i++) elem[i].style.width='0px';
 };
-
-document.addEventListener('click', (e)=>{
-    if(e.target===popupbg){
-        popupbg.classList.remove('active');
-        popupt.classList.remove('active');
-        popups.classList.remove('active');
-        popupf.classList.remove('active');
-    }
-});
-
